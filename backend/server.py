f84412cba7a3c7d4a7439f8424b8fa5100cd29b0
Sanjitha Ravi <200200022+sanjithar07@users.noreply.github.com>
2026-05-04 23:16:39 +0530

    syntax error

--- backend/server.py
+++ backend/server.py
@@ -31,7 +31,7 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:3000",
     "https://sanyam-engineering.vercel.app/"
-]
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
